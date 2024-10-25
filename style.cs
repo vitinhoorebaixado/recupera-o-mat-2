8e70a63a7ba504f73251bfb4bfb3b663bd2f88a7
vitinhoorebaixado <144128680+vitinhoorebaixado@users.noreply.github.com>
2024-10-25 09:41:09 -0300

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,9 @@
+ody {
+    background-color: #ffefd5; /* Cor de fundo pêssego */
+    font-family: 'Times New Roman', Times, serif;
+    color: #2f4f4f; /* Cor do texto em cinza escuro */
+}
+
+h1 {
+    color: #8b4513; /* Cor do título em marrom */
+}
